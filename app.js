0e2c489f6f0fe2e1c92de10cd0b5c7454c13d0b3
travellerCoder <guillermobukowski@gmail.com>
2023-01-04 20:30:08 -0300

    instalacion de ejs y creacion de carpeta partials

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ const app = express();
 
 const path = require('path');
 
-const nuevaCarpetaPath = path.resolve('public')
+const nuevaCarpetaPath = path.resolve('public');
 
 app.use(express.json());
 app.use(express.urlencoded());
+// se declara el uso de ejs
+app.set("view engine", "ejs");
 
-app.use(express.static(nuevaCarpetaPath)) 
-
+app.use(express.static(nuevaCarpetaPath));
+//creamos el puerto
 const port = process.env.PORT || 1126;
 app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
-
+// Route to home
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('views/home.html'))
 });
-
+// Route to register
 app.get('/registro', (req, res) => {
-    res.sendFile(path.resolve('views/register.html'));
+    res.sendFile(path.resolve('views/register.html'))
 });
-
+// Route to login
 app.get('/ingreso', (req, res) => {
-    res.sendFile(path.resolve('views/login.html'));
+    res.sendFile(path.resolve('views/login.html'))
 });
 
